ecb6da26082b472eb383ca12789ebda2b84f57a6
Mohammed Ou-zaa <ouzaa.contact@gmail.com>
2024-06-17 01:31:55 +0100

    feat: added auth and db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import MapWithSearch from "@/components/search-with-search";
-import { LayoutDashboard, LogIn } from "lucide-react";
+import { LayoutDashboard, LogIn, LogOut } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import dynamic from "next/dynamic";
+import { SignIn } from "@/components/signin-button";
+import { auth } from "@/auth";
+import { SignOut } from "@/components/signout-button";
 
-export default function Home() {
+const MapWithSearch = dynamic(() => import("@/components/map-with-search"), {
+  ssr: false,
+});
+
+export default async function Home() {
+  const session = await auth();
+  console.log(session?.user);
+
   return (
     <main className="flex">
       <div className="bg-blue-950 h-screen w-64 px-4 py-6 flex flex-col gap-6">
@@ -25,13 +35,17 @@ export default function Home() {
           <LayoutDashboard />
           <p>Dashboard</p>
         </Link>
-        <Link
-          href="#"
-          className="text-slate-300 text-md font-semibold flex items-end gap-1 hover:text-slate-500 transition flex-grow"
-        >
-          <LogIn />
-          <p>Log in</p>
-        </Link>
+        {session ? (
+          <div className="text-slate-300 text-md font-semibold flex items-end gap-1 hover:text-slate-500 transition flex-grow">
+            <LogOut />
+            <SignOut />
+          </div>
+        ) : (
+          <div className="text-slate-300 text-md font-semibold flex items-end gap-1 hover:text-slate-500 transition flex-grow">
+            <LogIn />
+            <SignIn />
+          </div>
+        )}
       </div>
       <MapWithSearch />
     </main>
